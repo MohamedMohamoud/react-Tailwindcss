ad698ce4abe4858775be65e0d1b509bbe0292ea8
mohamedmohamoud <mfrag228@gmail.com>
2024-02-15 13:09:28 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import RoundOne from "./components/Round/RoundOne";
+import RoundTwo from './components/Round/RoundTwo';
+import RoundThree from './components/Round/RoundThree';
+import RoundFour from './components/Round/RoundFour';
+import RoundFive from './components/Round/RoundFive';
+import Home from './Home';
+function App()  {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='w-full h-screen font-bodyFont text-textColor bg-black relative overflow-hidden '>
+      <div className='max-w-screen-2x1 h-full mx-auto flex justify-center items-center'>
+        <Home/>
+      </div>
+      <div className='w-full h-full absolute top-0 left-0 z-10'>
+      <RoundOne />
+      <RoundTwo />
+      <RoundThree />
+      <RoundFour />
+      <RoundFive />
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
